da323f1f4ee525fe4f3da2786c5c08032645429f
papu <p.k12sin@gmail.com>
2021-06-01 01:18:03 +0530

    feat: added 2 api getAllUsers and update password

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+// api for registration
 router.post('/v1/register', (req, res) => {
     let result ;
     if (!req.body.user_id) {
@@ -15,7 +16,7 @@ router.post('/v1/register', (req, res) => {
     if (!req.body.password) {
         result = {
             status: false,
-            message: "upassword can not be null. "
+            message: "password can not be null. "
         }
         res.status(400).send(result);
     }
@@ -43,6 +44,7 @@ router.post('/v1/register', (req, res) => {
     })
 });
 
+//api for login
 router.post('/v1/login', (req, res) => {
     let result;
     if (!req.body.user_id) {
@@ -56,14 +58,9 @@ router.post('/v1/login', (req, res) => {
     if (!req.body.password) {
         result = {
             status: false,
-            message: "upassword can not be null. "
+            message: "password can not be null. "
         }
         res.status(400).send(result);
-    }
-
-    let reqData = {
-        user_id: req.body.user_id,
-        password: req.body.password
     }
 
     User.findOne({
@@ -89,6 +86,76 @@ router.post('/v1/login', (req, res) => {
     })
 
 })
+
+//api for getting all users
+router.get('/v1/allUsers', (req, res) => {
+    let result;
+    User.findAll().then((value) => {
+        result = {
+            status: true,
+            message: "Success",
+            data: value
+        }
+        res.status(200).send(result);
+    }).catch(err => {
+        result = {
+            status: false,
+            message: `Error : ${err}`
+        }
+        res.status(500).send(result);
+    })
+})
+
+
+//api for updating password
+router.post('/v1/updatePassword', (req, res) => {
+    let result ;
+    if (!req.body.user_id) {
+        result = {
+            status: false,
+            message: "user id can not be null. "
+        }
+        res.status(400).send(result);
+    }
+
+    if (!req.body.password) {
+        result = {
+            status: false,
+            message: "password can not be null. "
+        }
+        res.status(400).send(result);
+    }
+    let newPassword = req.body.password;
+    User.update({password: newPassword}, {
+        where: {
+            user_id: req.body.user_id
+        }
+    }).then((value => {
+        result = {
+            status: true,
+            message: "password updated successfully",
+            data: {
+                user_id: req.body.user_id
+            }
+        }
+        if(value[0] === 1) {
+            res.status(200).send(result);
+        } else {
+            res.status(500).send({
+                status: false,
+                message:`can not update password with user_id = ${req.body.user_id}`
+            });
+        }
+        
+    })).catch(err => {
+        result = {
+            status: false,
+            message: `Error : ${err}`
+        }
+        res.status(500).send(result);
+    })
+})
+
 
 
 
